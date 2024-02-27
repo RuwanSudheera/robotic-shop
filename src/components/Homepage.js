692c13a30433dd749dbc02397285c27057db5dc6
RuwanSudheera <sudheerwim@gmail.com>
2024-02-27 23:47:57 +0530

    homepage searchbar development

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,13 +1,32 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import Items from './Items';
 
 const Homepage = ({ robots }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filter using material type
+  const filteredRobots = robots.filter((robot) => {
+    return robot.material.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div className="homepage">
       <h2>SL Robot Shop</h2>
+
+      {/* Search bar */}
+      <input
+        type="text"
+        placeholder="Search by material type"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
       <div className="row">
-        {robots.map((robot) => (
+      {filteredRobots.map((robot) => (
           <Items key={robot.id} robot={robot} />
         ))}
       </div>
